869a0744164a2068333fa780343956acb4bcfba1
zjnsteven <zhaojianing2011@gmail.com>
2016-06-01 10:45:35 -0400

    update output

--- demo/simInit.py
+++ demo/simInit.py
@@ -19,7 +19,7 @@ for c in range(0,size):
 		for i in range((iterations*rank/size + 1), (iterations*(rank+1)/size+1)):
 			print "Worker - rank %d on %s."%(rank, name) 
 			print "i:%d"%i
-			out_path = '/sciclone/home00/geogdan/OverlapC_TreeProp/sim_'+str(c)+'.csv'
+			out_path = '/sciclone/home00/geogdan/OverlapC_TreeProp/sim_'+str(i)+'.csv'
 			version = "1"
 			nrandom = "5000"#str(100 + random.random()*10000)
 			xvar_psill = ".1" #str(max(0.05,random.random()*1.0))
@@ -46,7 +46,12 @@ for c in range(0,size):
 			mod_error_psill ="0.1" #str(0.1 + random.random()*0.9)
 			trt_spill_sill = str(0.1 + random.random()*0.9)
 			
-			
+			outputList = [("version: ",version),("nrandom: ",nrandom),("xvar_psill: ",xvar_psill),("minx: ",minx),
+			("maxx: ",maxx),("miny: ", miny),("maxy: ",maxy),("var1_vrange: ",var1_vrange),("var1_error: ",var1_error),
+			("prop_acc: ",prop_acc),("var1_error_vrange: ",var1_error_vrange),("mod_error_magnitude: ",mod_error_magnitude),
+			("trt_prc: ", trt_prc), ("theta: ", theta),("beta: ", beta),("spill_vrange: ", spill_vrange), ("spill_magnitude: ",spill_magnitude),
+			("cal: ",cal), ("sample_size: ",sample_size),("tree_split_lim: ",tree_split_lim),("mod_error_vrange: ", mod_error_vrange),
+			("xvar_error_psill: ",xvar_error_psill), ("mod_error_psill: ", mod_error_psill),("trt_spill_sill: ", trt_spill_sill)]
 			try:
 			R_ret = check_output("Rscript " +
 			        sim_path + " " +
@@ -78,34 +83,16 @@ for c in range(0,size):
 			                  	stderr=STDOUT,
 			        shell=True)
 			
-			print c
+			print i
 			print R_ret
 			print "========================"
 			
 			except CalledProcessError as sts_err:                                                                                                   
-			print ">> subprocess error code:", sts_err.returncode, '\n', sts_err.output
-			print "ERROR: Insufficient Matches or another error occured in the R Script. Specifics unknown.  Good luck."
-			print version
-			print nrandom 
-			print xvar_psill 
-			print minx
-			print maxx 
-			print miny
-			print maxy
-			print var1_vrange
-			print var1_error
-			print prop_acc 
-			print var1_error_vrange 
-			print mod_error_magnitude 
-			print trt_prc 
-			print theta 
-			print beta 
-			print spill_vrange 
-			print spill_magnitude
-			print cal
-			print sample_size
-			print tree_split_lim
-			print mod_error_vrange
-			print xvar_error_psill 
-			print mod_error_psill 
-			print trt_spill_sill
+			# print ">> subprocess error code:", sts_err.returncode, '\n', sts_err.output
+			# print "ERROR: Insufficient Matches or another error occured in the R Script. Specifics unknown.  Good luck."
+			print outputList
+			
+			
+			
+			
+			
